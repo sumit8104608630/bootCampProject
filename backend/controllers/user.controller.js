ca0628f94d851d34eb122e1c486aad2f87824ce1
Rajiv Sharma <sharma9769387961@gmail.com>
2026-02-13 23:44:43 +0530

    smtp as not string sovled

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -184,7 +184,7 @@ const generateOtp = asyncHandler(async (req, res) => {
 
   // Send OTP to email
   const transporter = nodemailer.createTransport({
-    host: smtp.gmail.com,
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false, // true for 465, false for other ports
         auth: {
