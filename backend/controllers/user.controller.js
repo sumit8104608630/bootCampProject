6839df13da84f6ef6b6c91331c9256828048eda6
Rajiv Sharma <sharma9769387961@gmail.com>
2026-02-14 09:22:09 +0530

    trasporter rollback

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -188,16 +188,13 @@ const generateOtp = asyncHandler(async (req, res) => {
   }
 
   // Nodemailer transporter (Render-safe)
-  const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.SMTP_USERNAME,
-    pass: process.env.SMTP_PASSWORD,
-  },
-  family: 4, // 👈 FORCE IPv4 (IMPORTANT)
-});
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.COMPANY_EMAIL, // Avoid hardcoding email
+        pass: process.env.COMPANY_EMAIL_PASSWORD,
+      },
+    });
 
 
   // Email template
