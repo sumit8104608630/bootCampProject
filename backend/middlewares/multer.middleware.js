fd6cf01a2f98fa01e05067a41b740a9dca4ff1df
Sumit sharma <sumit8104608630@gmail.com>
2026-03-01 16:35:21 +0530

    final update

--- backend/middlewares/multer.middleware.js
+++ backend/middlewares/multer.middleware.js
@@ -1,15 +1,8 @@
 import multer from "multer";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Define __dirname manually in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-
-        cb(null, path.resolve(__dirname, "../public/temp")); // Use __dirname correctly
+        cb(null, "/tmp");
     },
     filename: (req, file, cb) => {
         const fileName = `${Date.now()}-${file.originalname}`;
